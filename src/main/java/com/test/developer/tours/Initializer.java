e2dc6527eab8e25bf5acc404e06c88d688787324
dxn8nis <dhamotharan_nanjappan@homedepot.com>
2019-08-19 16:09:05 -0400

    new spting boot proj

---
+++ src/main/java/com/test/developer/tours/Initializer.java
@@ -0,0 +1,39 @@
+package com.test.developer.tours;
+
+import com.test.developer.tours.model.Event;
+import com.test.developer.tours.model.Group;
+import com.test.developer.tours.repository.GroupRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.util.Collections;
+import java.util.stream.Stream;
+
+@Component
+class Initializer implements CommandLineRunner {
+
+    private final GroupRepository repository;
+
+    public Initializer(GroupRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void run(String... strings) {
+        Stream.of("Denver tour", "Utah tour", "Seattle tour",
+                "Richmond tour").forEach(name ->
+                repository.save(new Group(name))
+        );
+
+        Group djug = repository.findByName("Denver tour");
+        Event e = Event.builder().title("Full Stack Reactive")
+                .description("Reactive with Spring Boot + React")
+                .date(Instant.parse("2018-12-12T18:00:00.000Z"))
+                .build();
+        djug.setEvents(Collections.singleton(e));
+        repository.save(djug);
+
+        repository.findAll().forEach(System.out::println);
+    }
+}
